4343c9d67c75d70f8ebac37322e3394b0ed959c3
LukeNewsham <luke.newsham@outlook.com>
2022-07-20 21:11:28 +0100

    Update article data and styles

--- src/Components/Article.js
+++ src/Components/Article.js
@@ -4,14 +4,18 @@ import React from 'react';
 export default function Article(props) {
     return (
         <div class='articleContainer'>
-            <a href={"https:/www.reddit.com" + props.article.permalink}> 
-                <h1> {props.article.title} </h1>
-            </a>
-            <p> {'Subscribers: ' + props.article.subreddit}</p>
-            <p>{props.article.author}</p>
-            <p> {'Upvotes: ' + props.article.ups}</p>            
+            <div class='stats'>
+                <p> {'Upvotes: ' + props.article.ups}</p>                 
+            </div>
+
+            <div class='info'> 
+                <a href={"https:/www.reddit.com" + props.article.permalink}> 
+                    <h1> {props.article.title} </h1>
+                </a>                
+                <img width ='100%' src={props.article.url} alt=''/>  
+                <p class='author'>{'by ' + props.article.author}</p>    
+            </div>
             
-            {/* <h1> {JSON.parse(props.article)} </h1> */}
         </div>
     )
 }
